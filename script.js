cee2440c073c6a096d7410110062365bad241ca0
Mohammed Yousuf <mohammedyousuf16.md@gmail.com>
2024-10-10 20:04:52 +0530

    resetting the game

--- script.js
+++ script.js
@@ -12,15 +12,30 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const btnNew = document.querySelector('.btn--new');
 
-// Starting Conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+// Starting Conditions
+
+const init = function () {
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  scores[0] = 0;
+  scores[1] = 0;
+  player0El.classList.add('player--active');
+  document.querySelector(`.player--0`).classList.remove('player--winner');
+  document.querySelector(`.player--1`).classList.remove('player--winner');
+  diceEl.classList.add('hidden');
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+};
+init();
 
 const switchPlayer = function () {
   player0El.classList.toggle('player--active');
@@ -59,7 +74,7 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     //2.check if player's score is >=100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       diceEl.classList.add('hidden');
       document
@@ -75,3 +90,12 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+btnNew.addEventListener('click', init);
+//removing the winner screen
+// document
+//   .querySelector(`.player--${activePlayer}`)
+//   .classList.remove('player--winner');
+// // clear the player score and current score
+
+// // add the active player class
+// player0El.classList.toggle('player--active');
